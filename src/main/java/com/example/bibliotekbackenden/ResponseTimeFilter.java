44a66e59d4bf621ce658a5d3b56c4e69105ed937
Mariozalac716 <zalac.mario1@gmail.com>
2026-05-12 14:23:13 +0200

    Lägger till klass med metoden för att räkna på respons tiden, den loggar sedan tiden av varje anrop

---
+++ src/main/java/com/example/bibliotekbackenden/ResponseTimeFilter.java
@@ -0,0 +1,30 @@
+package com.example.bibliotekbackenden;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@Component
+public class ResponseTimeFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(ResponseTimeFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        long startTime = System.currentTimeMillis();
+        chain.doFilter(request, response);
+        long duration = System.currentTimeMillis() - startTime;
+        HttpServletRequest req = (HttpServletRequest) request;
+        
+        logger.info("Request URL: {}, Duration: {} ms", req.getRequestURL(), duration);
+    }   
+}
